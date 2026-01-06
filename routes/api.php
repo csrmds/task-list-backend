3c5b18d26bfbac86640b85d18e983eb053e1ffdb
Cesar Melo <csrmds@gmail.com>
2026-01-05 22:35:57 -0300

    update wakeup

--- routes/api.php
+++ routes/api.php
@@ -5,32 +5,39 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function (){
+	
+	Route::get('/user', function (Request $request) {
+		return $request->user();
+	});
 
-Route::get('/task/list', [App\Http\Controllers\TaskController::class, 'getTasks'])->middleware('auth:sanctum');
-Route::post('/task/store', [App\Http\Controllers\TaskController::class, 'store'])->middleware('auth:sanctum');
-Route::post('/task/destroy', [App\Http\Controllers\TaskController::class, 'destroy'])->middleware('auth:sanctum');
-Route::post('/task/update', [App\Http\Controllers\TaskController::class, 'update'])->middleware('auth:sanctum');
+	Route::get('/task/list', [App\Http\Controllers\TaskController::class, 'getTasks']);
+	Route::post('/task/store', [App\Http\Controllers\TaskController::class, 'store']);
+	Route::post('/task/destroy', [App\Http\Controllers\TaskController::class, 'destroy']);
+	Route::post('/task/update', [App\Http\Controllers\TaskController::class, 'update']);
+	
+	Route::get('/auth/logout', [App\Http\Controllers\UserController::class, 'logout']);
+	Route::get('/auth/check', [App\Http\Controllers\UserController::class, 'authCheck']);
+
+	Route::post('/gcalendar/createevent', [App\Http\Controllers\GoogleCalendarController::class, 'createEvent']);
+	Route::post('/gcalendar/updateevent', [App\Http\Controllers\GoogleCalendarController::class, 'updateEvent']);
+	Route::post('/gcalendar/deleteevent', [App\Http\Controllers\GoogleCalendarController::class, 'deleteEvent']);
+
+});
+
 
 Route::post('/auth/login', [App\Http\Controllers\UserController::class, 'login']);
-Route::get('/auth/logout', [App\Http\Controllers\UserController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/auth/check', [App\Http\Controllers\UserController::class, 'authCheck'])->middleware('auth:sanctum');
-
-Route::post('/gcalendar/createevent', [App\Http\Controllers\GoogleCalendarController::class, 'createEvent'])->middleware('auth:sanctum');
-Route::post('/gcalendar/updateevent', [App\Http\Controllers\GoogleCalendarController::class, 'updateEvent'])->middleware('auth:sanctum');
-Route::post('/gcalendar/deleteevent', [App\Http\Controllers\GoogleCalendarController::class, 'deleteEvent'])->middleware('auth:sanctum');
 
 
 Route::get('/wakeup', function() {
 	try {
-		$result= DB::select('select now() as now');
+		//$result= DB::select('select now() as now');
+		$result= DB::select('select count(*), now() from migrations');
 
 		return response()->json([
 			'status'=> 'ok',
-			'database_time'=> $result[0]->now ?? null
+			'response'=> $result[0] ?? null
 		]);
 	} catch(\Exception $e) {
 		return response()->json([
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/auth/google', [App\Http\Controllers\UserController::class, 'authGoogle']);
 Route::get('/auth/callback/google', [App\Http\Controllers\UserController::class, 'authCallback']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
